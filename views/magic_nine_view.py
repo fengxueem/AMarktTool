0f91f4e2851fb304eca8ea5a50539af18d645165
Yinan Fu <fengxueem@gmail.com>
2024-05-29 22:55:00 +0800

    add scrollbar

--- views/magic_nine_view.py
+++ views/magic_nine_view.py
@@ -8,7 +8,7 @@ class MagicNineView(CTkFrame):
         super().__init__(*arg, **kwargs)
 
         # 不使用 Treeview 控件提供的表头
-        self.table = ttk.Treeview(self, show='headings', height=5)
+        self.table = ttk.Treeview(self, show='headings')
         # 自定义表头
         columns = {
             TABLE_STOCK_CODE: TABLE_WIDTH_STOCK_CODE, 
@@ -19,4 +19,12 @@ class MagicNineView(CTkFrame):
         for c in columns:
             self.table.heading(c, text = c)
             self.table.column(c, width = columns[c], anchor='center')          
-        self.table.pack()
+        self.table.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
+        
+        # 创建垂直滚动条
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical")
+        self.scrollbar.grid(row=0, column=1, sticky='nsw')
+        
+        # 关联滚动条和表格
+        self.table.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.table.yview)
