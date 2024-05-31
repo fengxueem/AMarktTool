3b9d130660b446a7a8b30bbcc66b6926f8714a03
Yinan Fu <fengxueem@gmail.com>
2024-05-31 17:09:54 +0800

    make magic nine faster

--- controllers/magic_nine_controller.py
+++ controllers/magic_nine_controller.py
@@ -1,21 +1,25 @@
 import akshare as ak
 from datetime import datetime, timedelta
+from multiprocessing import Pool
 
 from config import MAGIC_NINE_FRAME, TABLE_STOCK_CODE, TABLE_STOCK_NAME
+from config import AK_DATE_FORMAT, AK_DATAFRAME_DATE, AK_DATAFRAME_CLOSING_PRICE
 
 from views import View
 from models import Model
 
 class MagicNineController:
+    
     def __init__(self, view : View, model : Model) -> None:
         self.view = view
         self.model = model
         self.frame = self.view.frames[MAGIC_NINE_FRAME]
         self.all_stocks = model.magic_nine_model.all_stocks
+        self.pool = Pool(processes=16)
     
     @staticmethod
-    def bigger_than_4_days_later(dataframe_from_ak, row):
-        if dataframe_from_ak.loc[row, '收盘'] > dataframe_from_ak.loc[row + 4, '收盘']:
+    def bigger_than_x_days_later(dataframe_from_ak, x, row):
+        if dataframe_from_ak.loc[row, AK_DATAFRAME_CLOSING_PRICE] > dataframe_from_ak.loc[row + x, AK_DATAFRAME_CLOSING_PRICE]:
             return True
         else:
             return False
@@ -27,7 +31,6 @@ class MagicNineController:
     def find_magic_number(bool_array, magic_number):
         sum_array = [0] * len(bool_array)
         # 初始化变量来记录连续True的起始下标
-        last_index = -1
         count = 0  # 连续True的计数器
 
         # 遍历布尔数组
@@ -45,39 +48,59 @@ class MagicNineController:
                 return i  # 返回找到的下标
         return -1  # 如果没有找到，则返回-1
 
+    @staticmethod
+    def process_stock(stock, start_date_str, end_date_str):
+        # 查询这只股票的往日价格
+        stock_zh_a_hist_df = ak.stock_zh_a_hist(
+            symbol=stock[TABLE_STOCK_CODE],
+            period="daily",
+            start_date=start_date_str,
+            end_date=end_date_str,
+            adjust="hfq"
+            )
+        # 生成一个每日价格是否超过当日后第4天的布尔数组，用于判断神奇 X 转
+        daily_result = []
+        magic_number = 9
+        half_magic_num = magic_number // 2
+        for entry in range(0, stock_zh_a_hist_df.shape[0] - magic_number // 2):
+            daily_result.append(
+                MagicNineController.bigger_than_x_days_later(
+                    dataframe_from_ak = stock_zh_a_hist_df,
+                    x = half_magic_num,
+                    row = entry
+                    )
+                )
+        # 查询神奇 X 转是否存在
+        magic_turn_index = MagicNineController.find_magic_number(daily_result, magic_number)
+        # 如果存在则将其放入数据表格中
+        if magic_turn_index > -1:
+            signal_date = stock_zh_a_hist_df.loc[magic_turn_index, AK_DATAFRAME_DATE]
+            return (stock[TABLE_STOCK_CODE], stock[TABLE_STOCK_NAME], signal_date)
+        return None
+
 
     def init_table(self) -> None:
         # 获取今天的日期和时间
         today = datetime.now()
-        today_str = today.strftime("%Y%m%d")
-        # # 计算约一个月前的日期
-        one_month_ago = (today - timedelta(days=28))
-        one_month_ago_str = one_month_ago.strftime("%Y%m%d")
-        valid_magic_count = 0
+        today_str = today.strftime(AK_DATE_FORMAT)
+        # 计算约一个月前的日期
+        one_month_ago = (today - timedelta(days=27))
+        one_month_ago_str = one_month_ago.strftime(AK_DATE_FORMAT)
+        
+        valid_magic_list = []
         for _, stock in enumerate(self.all_stocks):
-            # 查询这只股票的往日价格
-            stock_zh_a_hist_df = ak.stock_zh_a_hist(
-                symbol=stock[TABLE_STOCK_CODE],
-                period="daily",
-                start_date=one_month_ago_str,
-                end_date=today_str,
-                adjust="hfq"
-                )
-            # 生成一个每日价格是否超过当日后第4天的布尔数组，用于判断神奇 X 转
-            daily_result = []
-            for entry in range(0, stock_zh_a_hist_df.shape[0] - 4):
-                daily_result.append(
-                    self.bigger_than_4_days_later(
-                        dataframe_from_ak = stock_zh_a_hist_df,
-                        row = entry
-                        )
-                    )
-            # 查询神奇 X 转是否存在
-            magic_turn_index = self.find_magic_number(daily_result, 9)
-            # 如果存在则将其放入数据表格中
-            if magic_turn_index > -1:
+            # 创建并启动进程
+            result = self.pool.apply_async(MagicNineController.process_stock, (stock, one_month_ago_str, today_str))
+            valid_magic_list.append(result)
+
+        # 关闭进程池，不添加新的进程
+        self.pool.close()
+        # 等待所有进程完成
+        self.pool.join()
+        valid_magic_count = 0
+        for valid_magic_stock in valid_magic_list:
+            res = valid_magic_stock.get()
+            if res is not None:
                 valid_magic_count += 1
-                signal_date = stock_zh_a_hist_df.loc[magic_turn_index, '日期']
-                values = (valid_magic_count, stock[TABLE_STOCK_CODE], stock[TABLE_STOCK_NAME], signal_date)
-                self.frame.table.insert('', 'end', values = values)
+                self.frame.table.insert('', 'end', values = ((valid_magic_count,) + res))                
         
