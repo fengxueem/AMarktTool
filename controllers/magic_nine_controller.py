532b58d15ea72da55dd71afec5d4a0563ad26a0f
Yinan Fu <fengxueem@gmail.com>
2024-05-31 23:29:24 +0800

    fix a bug view should not have logical methods

--- controllers/magic_nine_controller.py
+++ controllers/magic_nine_controller.py
@@ -1,5 +1,6 @@
 from config import MAGIC_NINE_FRAME
 from config import MAGIC_NINE_BTN, MAGIC_NINE_BTN_DISABLED
+from config import TABLE_INDEX, TABLE_STOCK_CODE, TABLE_SIGNAL_DATE
 from models import Model
 from views import View
 
@@ -7,10 +8,49 @@ import threading
 
 class MagicNineController:
     
+    @staticmethod
+    def table_sort_index(table, col, is_reverse_order):
+        lines = [(table.set(k, col), k) for k in table.get_children('')]
+        lines.sort(key=lambda t: int(t[0]), reverse=is_reverse_order)
+        # 移动数据
+        for index, (_, key) in enumerate(lines):
+            table.move(key, '', index)
+        # 下一次点击表头时，使用相反的顺序排序
+        table.heading(col, command=lambda: MagicNineController.table_sort_index(table, col, not is_reverse_order))
+    
+    @staticmethod
+    def table_sort_stock_code(table, col, is_reverse_order):
+        lines = [(table.set(k, col), k) for k in table.get_children('')]
+        lines.sort(key=lambda t: int(t[0]), reverse=is_reverse_order)
+        # 移动数据
+        for index, (_, key) in enumerate(lines):
+            table.move(key, '', index)
+        # 下一次点击表头时，使用相反的顺序排序
+        table.heading(col, command=lambda: MagicNineController.table_sort_stock_code(table, col, not is_reverse_order))
+    
+    @staticmethod
+    def table_sort_signal_date(table, col, is_reverse_order):
+        lines = [(table.set(k, col), k) for k in table.get_children('')]
+        lines.sort(key=lambda t: int(t[0].replace('-', '')), reverse=is_reverse_order)
+        # 移动数据
+        for index, (_, key) in enumerate(lines):
+            table.move(key, '', index)
+        # 下一次点击表头时，使用相反的顺序排序
+        table.heading(col, command=lambda: MagicNineController.table_sort_signal_date(table, col, not is_reverse_order))
+    
     def __init__(self, view : View, model : Model) -> None:
         self.view = view
         self.model = model
         self.frame = self.view.frames[MAGIC_NINE_FRAME]
+        # 自定义表格每列排序方法
+        columns = {
+            TABLE_INDEX : [MagicNineController.table_sort_index],
+            TABLE_STOCK_CODE : [MagicNineController.table_sort_stock_code], 
+            TABLE_SIGNAL_DATE : [MagicNineController.table_sort_signal_date]
+        }
+        for c in columns:
+            sort_func = columns[c][0]
+            self.frame.table.heading(c, command=lambda _col=c: sort_func(self.frame.table, _col, False))      
         # start calculating magic nine as soon as the app started
         t = threading.Thread(target=self.init_table, args=())
         t.start()
--- views/magic_nine_view.py
+++ views/magic_nine_view.py
@@ -4,38 +4,7 @@ from config import TABLE_WIDTH_INDEX, TABLE_WIDTH_STOCK_CODE, TABLE_WIDTH_STOCK_
 from customtkinter import CTkFrame, CTkScrollbar
 import tkinter.ttk as ttk
 
-class MagicNineView(CTkFrame):
-    
-    @staticmethod
-    def table_sort_index(table, col, is_reverse_order):
-        lines = [(table.set(k, col), k) for k in table.get_children('')]
-        lines.sort(key=lambda t: int(t[0]), reverse=is_reverse_order)
-        # 移动数据
-        for index, (_, key) in enumerate(lines):
-            table.move(key, '', index)
-        # 下一次点击表头时，使用相反的顺序排序
-        table.heading(col, command=lambda: MagicNineView.table_sort_index(table, col, not is_reverse_order))
-    
-    @staticmethod
-    def table_sort_stock_code(table, col, is_reverse_order):
-        lines = [(table.set(k, col), k) for k in table.get_children('')]
-        lines.sort(key=lambda t: int(t[0]), reverse=is_reverse_order)
-        # 移动数据
-        for index, (_, key) in enumerate(lines):
-            table.move(key, '', index)
-        # 下一次点击表头时，使用相反的顺序排序
-        table.heading(col, command=lambda: MagicNineView.table_sort_stock_code(table, col, not is_reverse_order))
-    
-    @staticmethod
-    def table_sort_signal_date(table, col, is_reverse_order):
-        lines = [(table.set(k, col), k) for k in table.get_children('')]
-        lines.sort(key=lambda t: int(t[0].replace('-', '')), reverse=is_reverse_order)
-        # 移动数据
-        for index, (_, key) in enumerate(lines):
-            table.move(key, '', index)
-        # 下一次点击表头时，使用相反的顺序排序
-        table.heading(col, command=lambda: MagicNineView.table_sort_signal_date(table, col, not is_reverse_order))
-    
+class MagicNineView(CTkFrame):    
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
 
@@ -48,12 +17,12 @@ class MagicNineView(CTkFrame):
             show='headings', # 隐藏表格默认的首列
             yscrollcommand=self.scrollbar.set # 关联表格 -> 滚动条
             )
-        # 自定义每列 最小宽度 和 排序方法
+        # 自定义每列最小宽度
         columns = {
-            TABLE_INDEX : [TABLE_WIDTH_INDEX, MagicNineView.table_sort_index],
-            TABLE_STOCK_CODE : [TABLE_WIDTH_STOCK_CODE, MagicNineView.table_sort_stock_code], 
+            TABLE_INDEX : [TABLE_WIDTH_INDEX],
+            TABLE_STOCK_CODE : [TABLE_WIDTH_STOCK_CODE], 
             TABLE_STOCK_NAME : [TABLE_WIDTH_STOCK_NAME],
-            TABLE_SIGNAL_DATE : [TABLE_WIDTH_SIGNAL_DATE, MagicNineView.table_sort_signal_date]
+            TABLE_SIGNAL_DATE : [TABLE_WIDTH_SIGNAL_DATE]
         }
         # 批量设置表头标题
         self.table['columns'] = list(columns)
@@ -62,10 +31,6 @@ class MagicNineView(CTkFrame):
             self.table.heading(c, text = c)
             # 设置当前列宽度与其他格式
             self.table.column(c, minwidth = columns[c][0], anchor='center')
-            # 如果当前列存在排序方法，则将其添加
-            if len(columns[c]) > 1:
-                sort_func = columns[c][1]
-                self.table.heading(c, command=lambda _col=c: sort_func(self.table, _col, False))      
         
         # 关联滚动条 -> 表格 
         self.scrollbar.configure(command=self.table.yview)
