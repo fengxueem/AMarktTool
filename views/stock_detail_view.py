9123d5b90efc4dbc99f7ff8803231c68e01b5061
Yinan Fu <fengxueem@gmail.com>
2024-06-04 15:17:25 +0800

    use grid layout in stock detail view

--- views/stock_detail_view.py
+++ views/stock_detail_view.py
@@ -5,13 +5,31 @@ from matplotlib.figure import Figure
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 
+class DateFrame(CTkFrame):
+    def __init__(self, master, label_text):
+        super().__init__(master)
+
+        self.date_label = ctk.CTkLabel(self, text=label_text)
+        self.date_label.grid(row = 0,column = 0, sticky="nw")
+        self.date_entry = ctk.CTkEntry(self, placeholder_text="YYYY-MM-DD")
+        self.date_entry.grid(row = 1,column = 0, sticky="nw")
+
 class StockDetailView(CTkFrame):    
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
-        
+        # let child widgets use the whole window space
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        # 创建日期输入控件和刷新按钮
+        self.start_date_frame = DateFrame(self, "开始日期:")
+        self.start_date_frame.grid(row = 0, column = 0)
+        self.end_date_frame = DateFrame(self, "结束日期:")
+        self.end_date_frame.grid(row = 0, column = 1)        
         # 创建刷新按钮
         self.refresh_button = CTkButton(self, text="刷新")
-        self.refresh_button.pack(pady=20)
+        self.refresh_button.grid(row = 0, column = 2)
         
         # 创建 matplotlib 图表
         self.fig = Figure()
@@ -24,10 +42,7 @@ class StockDetailView(CTkFrame):
         
         # 将图表嵌入到 customtkinter 窗口中
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)        
-        # 添加 matplotlib 导航工具栏
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
-        self.toolbar.update()
-        self.canvas._tkcanvas.pack(fill=ctk.BOTH, expand=True)        
+        self.canvas._tkcanvas.grid(row = 1,column = 0, sticky="nsew", columnspan=3)
     
         # 创建注释文本，初始时不可见
         self.annot = self.ax.annotate("", xy=(0,0), xytext=(20,20), textcoords="offset points",
